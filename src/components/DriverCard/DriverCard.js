11e232d32c041dd42d670d782da063cc427d3a49
Raúl <raul.alonso.merino@gmail.com>
2022-03-16 16:18:43 +0100

    finished iteration 7

---
+++ src/components/DriverCard/DriverCard.js
@@ -0,0 +1,23 @@
+import "./DriverCard.css";
+import Rating from "../Rating/Rating";
+
+function DriverCard(props) {
+  return (
+    <div className="driverCard_container">
+      <img src={props.img} alt={props.img} />
+      <div className="info">
+        <p className="infoName">
+          <b>{props.name}</b>
+        </p>
+        <p>
+          <Rating children={props.rating} />
+        </p>
+        <p className="infoCar">
+          {props.car.model} - {props.car.licensePlate}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default DriverCard;
---
+++ src/components/Rating/Rating.js
@@ -0,0 +1,36 @@
+import "./Rating.css";
+//import { BsStarFill, BsStar, BsStarHalf } from "react-icons/bs";
+
+function Rating(props) {
+  const stars = (num) => {
+    if (num >= 0.5 && num < 1.5) {
+      return "★☆☆☆☆";
+    } else if (num >= 1.5 && num < 2.5) {
+      return "★★☆☆☆";
+    } else if (num >= 2.5 && num < 3.5) {
+      return "★★★☆☆";
+    } else if (num >= 3.5 && num < 4.5) {
+      return "★★★★☆";
+    } else if (num >= 4.5) {
+      return "★★★★★";
+      /* return (
+        <p>
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+        </p>
+      ); */
+    } else {
+      return "☆☆☆☆☆";
+    }
+  };
+  return (
+    <div className="stars_container">
+      <p>{stars(props.children)}</p>
+    </div>
+  );
+}
+
+export default Rating;
